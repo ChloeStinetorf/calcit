fa7596ea18adfcb36ffebc1d199ff9a721e21a85
Chloe Stinetorf <chloe.stinetorf@gmail.com>
2013-01-29 15:20:17 -0500

    basic calc added

---
+++ main.rb
@@ -0,0 +1,32 @@
+def add(first_number, second_number)
+  print "What is the first number?"
+  first_number = gets.chomp
+  print "What is the second number?"
+  second_number = gets.chomp
+  first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  print "What is the first number?"
+  first_number = gets.chomp
+  print "What is the second number?"
+  second_number = gets.chomp
+  first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  print "What is the first number?"
+  first_number = gets.chomp
+  print "What is the second number?"
+  second_number = gets.chomp
+  first_number * second_number
+end
+
+def divide(first_number, second_number)
+  print "What is the dividend?"
+  first_number = gets.chomp
+  print "What is the divisor?"
+  second_number = gets.chomp
+  first_number / second_number
+end
+
