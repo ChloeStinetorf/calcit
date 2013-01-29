9ed1875ea0122eb9ef99752a3aff7fa60ec4c84a
Blanchet <h55nick@gmail.com>
2013-01-29 15:42:25 -0500

    added looping

--- main.rb
+++ main.rb
@@ -34,31 +34,48 @@ def filterselection(input)
   exit = false
   if (input == "1" || input.downcase == "b")
     #Call function for BASIC
-    print "1/b"
+    print "\n1/b\n"
   elsif (input == '2' || input.downcase == "a")
     #Call function for Advanced
-    print "2/a"
+    print "\n2/a\n"
   elsif(input == "3" || input.downcase == "e")
     #exit
     exit = true
+    print "\n\n ****************\n \n GOOD BYE \n\n ****************"
   else
     print "\n***\n You have not entered a valid choice. \n***\n"
   end
 exit
 end
 
+def options()
+  print "\n What type of calculator would you like to use?\n"
+  print "(1 or B  => Basic Calculator)\n"
+  print "(2 or A  => Advanced Calculator)\n"
+  print "(3 or E  => Exit)\n\n"
+end
 
-print "\n\n\nWelcome to the most amazing calculator. \n"
+
+########     Welcome Code ###############
+print "\n\n\nWelcome to the most amazing calculator\n"
 print "\n*****************************************\n"
-print "\n What type of calculator would you like to use?\n"
-print "(1 or B  => Basic Calculator)\n"
-print "(2 or A  => Advanced Calculator)\n"
-print "(3 or E  => Exit)\n\n"
-
+options()
+########  Initial Selection
 print "Selection: "
 selection = gets.chomp
 
-print filterselection(selection)
+#Set to Loop
+exit = false
+
+#### Loop until Exit!  (this is where you could to until(exit)!!!  )
+while(!exit)
+  exit = filterselection(selection)
+  if (!exit)
+    options()
+    selection = gets.chomp
+  end
+end
+
 
 
 print "\n\n\n\n"
