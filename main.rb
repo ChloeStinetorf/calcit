143458d784807a269e612f553658f0715b2cb371
Blanchet <h55nick@gmail.com>
2013-01-29 18:51:24 -0500

    major changes

--- main.rb
+++ main.rb
@@ -1,21 +1,52 @@
 require 'rainbow'
 ### Simon ###
+
+def trip_calc()
+      ##trip inputs
+      print `clear`
+      print "Please input the Distance you will travel: (miles) "
+      dis = gets.chomp.to_i
+      print "Please input the MPG your car gets:  "
+      mpg = gets.chomp.to_i
+      print "Please input the price per gallon:  "
+      ppg = gets.chomp.to_f
+      print "What is your average planned speed (MPH):  "
+      aps = gets.chomp.to_f
+
+    ##adjusting MPG
+    if aps <= 60
+      mpg = mpg # AKA do NOTHING
+    else
+      mpg = mpg - (2*(aps-60))
+      if mpg <= 0
+        print "You have negative MPG because you are a speed demon with a shity car. \n"
+      end
+    end
+
+    print "It's going to take #{aps*dis} hours and will cost you $ #{mpg*dis*ppg}\n\n"
+   gc
+end
+
+def gc()
+    gets
+    print `clear`
+end
+
+
 ### BMI ###
-
-
 def bmi(lbs,inch)
   (lbs/(inch * inch))*703
 end
 
 def find_bmi()
   puts `clear`
-  print "Let me help you find out your Body Mass Index, how much do you weight?"
+  print "Let me help you find out your Body Mass Index, how much do you weight?  "
    lbs = gets.chomp.to_f
-  print "Now tell me how tall are you in inches?"
+  print "Now tell me how tall are you in inches?  "
   inch = gets.chomp.to_f
-  print "Your BMI is " + bmi(lbs, inch).to_s
+  print "\nYour BMI is " + bmi(lbs, inch).to_s + "\n"
+  gc
 end
-
 
 def advanced_calc()
   print "\nWhich option would you like? square(s) or power(p)? Please type in one (s or p)  \n".foreground(:blue)
@@ -36,6 +67,7 @@ def advanced_calc()
   else
     print "\n\n\n Error In Advanced Input\n\n\n".foreground(:red)
   end
+   gc
 end
 
 def square(x)
@@ -121,7 +153,7 @@ def filterselection(input)
 
   elsif (input == "5" || input.downcase == "c")
     #Car Trip Info
-
+    trip_calc()
   elsif(input == "6" || input.downcase == "e")
 
     #exit
@@ -139,7 +171,7 @@ def options()
   print "(2 or A  => Advanced Calculator)\n".foreground(:blue)
   print "(3 or I   => Calculate BMI)\n".foreground(:blue)
   print "(4 or M => Calculate Morgage)\n".foreground(:blue)
-  print "(5 or C  => Car Trip Info)\n ".foreground(:blue)
+  print "(5 or C  => Car Trip Info)\n".foreground(:blue)
   print "(6 or E  => Exit)\n\n".foreground(:blue)
 end
 
@@ -164,11 +196,3 @@ while(!exit)
     selection = gets.chomp
   end
 end
-
-
-
-# print "\n\n\n\n"
-
-
-
-
